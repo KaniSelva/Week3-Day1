3a0cf81bae20c68e0ff186df10c69b960814fbc0
KaniSelva <90255979+KaniSelva@users.noreply.github.com>
2021-09-22 00:41:40 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,17 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public void desktopsize() {
+System.out.println("The Size of desktop is 8GB");
+	}
+
+	public static void main(String[] args) {
+		Desktop obj = new Desktop();
+		obj.computerModel();
+		obj.desktopsize();
+
+			
+		
+	}
+
+}
